41ca35f97c239181817cda1cdc03ddcf0c3bf9d5
ancgarciamo <51928175+ancgarciamo@users.noreply.github.com>
2022-10-17 15:56:50 -0500

    Add files via upload

--- TAE2.py
+++ TAE2.py
@@ -9,140 +9,38 @@ from sklearn.feature_selection import f_classif
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.stats import chi2_contingency
+import streamlit as st
+import lzma
+import dill as pickle
+
 ref_categories = ['mths_since_last_credit_pull_d:>75', 'mths_since_issue_d:>122', 'mths_since_earliest_cr_line:>434', 'total_rev_hi_lim:>79,780',
                   'total_rec_int:>7,260', 'total_pymnt:>25,000', 'out_prncp:>15,437', 'revol_util:>1.0', 'inq_last_6mths:>4', 'dti:>35.191',
                   'annual_inc:>150K', 'int_rate:>20.281', 'term:60', 'purpose:major_purch__car__home_impr', 'verification_status:Not Verified',
                   'home_ownership:MORTGAGE', 'grade:G']
 
-data1=[{'term':36,
-   'int_rate':14.64,
-   'grade':'C',
-   'emp_length':10.0,
-   'home_ownership' :'OWN',
-   'annual_inc':50000.0,
-   'verification_status':'Source Verified',
-   'purpose':'home_improvement',
-   'dti':19.11,
-   'inq_last_6mths':4.0,
-   'revol_util':49.0,
-   'total_acc':32.0,
-   'out_prncp':0.0,
-   'total_pymnt':2090.92,
-   'total_rec_int':290.92,
-   'last_pymnt_amnt':1159.57,
-   'tot_cur_bal':107437.0,
-   'total_rev_hi_lim':2450.0,
-   'mths_since_earliest_cr_line':347.0,
-   'mths_since_issue_d':75.0,
-   'mths_since_last_pymnt_d':59.0,
-   'mths_since_last_credit_pull_d':55.0,
-   'grade:A':0,
-   'grade:B':0,
-   'grade:C':1,
-   'grade:D':0,
-   'grade:E':0,
-   'grade:F':0,
-   'grade:G':0,
-   'home_ownership:MORTGAGE':0,
-   'home_ownership:NONE':0,
-   'home_ownership:OTHER':0,
-    'home_ownership:OWN':1,
-   'home_ownership:RENT':0,
-   'verification_status:Not Verified':0,
-   'verification_status:Source Verified':1,
-   'verification_status:Verified':0,
-   'purpose:car':0,
-   'purpose:credit_card':0,
-   'purpose:debt_consolidation':0,
-   'purpose:educational':0,
-   'purpose:home_improvement':1,
-   'purpose:house':0,
-   'purpose:major_purchase':0,
-   'purpose:medical':0,
-   'purpose:moving':0,
-   'purpose:other':0,
-   'purpose:renewable_energy':0,
-   'purpose:small_business':0,
-   'purpose:vacation':0,
-   'purpose:wedding':0
-}]
-data2=[{'term':36,
-   'int_rate':7.12,
-   'grade':'A',
-   'emp_length':1.0,
-   'home_ownership' :'RENT',
-   'annual_inc':63000.0,
-   'verification_status':'Not Verified',
-   'purpose':'debt_consolidation',
-   'dti':7.98,
-   'inq_last_6mths':0.0,
-   'revol_util':7.1,
-   'total_acc':24.0,
-   'out_prncp':2992.57,
-   'total_pymnt':3526.4,
-   'total_rec_int':518.97,
-   'last_pymnt_amnt':185.6,
-   'tot_cur_bal':4413.0,
-   'total_rev_hi_lim':27000.0,
-   'mths_since_earliest_cr_line':265.0,
-   'mths_since_issue_d':74.0,
-   'mths_since_last_pymnt_d':55.0,
-   'mths_since_last_credit_pull_d':55.0,
-   'grade:A':1,
-   'grade:B':0,
-   'grade:C':0,
-   'grade:D':0,
-   'grade:E':0,
-   'grade:F':0,
-   'grade:G':0,
-   'home_ownership:MORTGAGE':0,
-   'home_ownership:NONE':0,
-   'home_ownership:OTHER':0,
-    'home_ownership:OWN':0,
-   'home_ownership:RENT':1,
-   'verification_status:Not Verified':1,
-   'verification_status:Source Verified':0,
-   'verification_status:Verified':0,
-   'purpose:car':0,
-   'purpose:credit_card':0,
-   'purpose:debt_consolidation':1,
-   'purpose:educational':0,
-   'purpose:home_improvement':0,
-   'purpose:house':0,
-   'purpose:major_purchase':0,
-   'purpose:medical':0,
-   'purpose:moving':0,
-   'purpose:other':0,
-   'purpose:renewable_energy':0,
-   'purpose:small_business':0,
-   'purpose:vacation':0,
-   'purpose:wedding':0
-}]
+@st.cache
+def load_modelB():
+    with lzma.open('prediccion_compressed.pkl', 'rb') as file:
+        B = pickle.load(file)
+        return B
 
-dfxxx = pd.DataFrame(data2)
-import lzma
+B = load_modelB()
 
-import dill as pickle
-with lzma.open('prediccion_compressed.pkl', 'rb') as file:
-    B = pickle.load(file)
 
-with lzma.open('df_scorecard.pkl', 'rb') as file:
-   A = pickle.load(file)
+@st.cache
+def load_modelA():
+    with lzma.open('df_scorecard.pkl', 'rb') as file:
+        A = pickle.load(file)
+        return A
+A = load_modelA()
 
-with lzma.open('woe_transform.pkl', 'rb') as file:
-   C = pickle.load(file)
+@st.cache
+def load_modelC():
+    with lzma.open('woe_transform.pkl', 'rb') as file:
+        C = pickle.load(file)
+        return C
 
-X_test_woe_transformed = C.fit_transform(dfxxx)
-X_test_woe_transformed.insert(0, 'Intercept', 1)
-scorecard_scores = A['Score - Final']
-X_test_woe_transformed = pd.concat([X_test_woe_transformed, pd.DataFrame(dict.fromkeys(ref_categories, [0] * len(X_test_woe_transformed)),
-                                                                         index = X_test_woe_transformed.index)], axis = 1)
-# Need to reshape scorecard_scores so that it is (102,1) to allow for matrix dot multiplication
-scorecard_scores = scorecard_scores.values.reshape(102, 1)
-y_scores = X_test_woe_transformed.dot(scorecard_scores)
-print("Su puntaje es :" + str(y_scores[0][0]))
-
-import streamlit as st
+C = load_modelC()
 
 
 def main():
@@ -179,11 +77,11 @@ def main():
    purpose_vacation = 0
    purpose_wedding = 0
 
-   st.title("Encuesta de credito")
+   st.title("Calcular tarjeta de puntuación")
    termino=st.radio("Escoja el termino",(36,60))
-   int_ratee=st.number_input('Ingrese la tasa de interes del prestamo')
+   int_ratee=st.number_input('Ingrese la tasa de interes del préstamo')
    grado=st.radio("Escoja el grado",('A','B','C','D','E','F','G'))
-   emp_l=st.slider("Numero de años trabajando",0,10,1)
+   emp_l=st.slider("Número de años trabajando",0,10,1)
    ingresos_anuales=st.number_input('Ingrese la cantidad de sus ingresos anuales (En dolares)')
    casa=st.radio("Cual es su estado de posesion de vivienda",('Hipoteca','No posee','Otro','Casa propia','Arrendado'))
    ver_sta=st.radio("ss",('No verificado','Fuente verificada','Verificado'))
@@ -195,7 +93,7 @@ def main():
 
    dtii = st.number_input("Ingrese su dti (Una relación calculada utilizando los pagos de deuda mensuales totales del prestatario sobre las obligaciones de deuda total, excluyendo la hipoteca y el préstamo LC solicitado, dividido por los ingresos mensuales autoinformados del prestatario.)")
    inq_6meses = st.number_input("Ingrese el número de consultas en los últimos 6 meses (excluyendo consultas de automóviles e hipotecas")
-   revol = st.number_input("Cantidad del credito que el prestatartio esta utilizando en relacion con todo el credito giratorio disponible")
+   revol = st.number_input("Cantidad del crédito que el prestatario está utilizando en relación con todo el crédito giratorio disponible")
    totalacc = st.number_input("Ingrese el número total de líneas de crédito actualmente en el archivo de crédito del prestatario")
    out_pri = st.number_input("Capital restante pendiente por el monto total financiado")
    pago_total = st.number_input("Ingrese el total de  pagos hasta la fecha")
@@ -203,10 +101,10 @@ def main():
    ultimo_pago = st.number_input("Ingrese el último monto total de pago recibido")
    total_cur = st.number_input("Ingrese su saldo actual total de todas las cuentas")
    total_rev_lims = st.number_input("Límite total de crédito/crédito de alto aumento giratorio")
-   meses_cr = st.number_input("Numero de meses desde que se abrió la línea de crédito más temprana del prestatario")
-   meses_issue = st.number_input("Numero de meses desde que el prestamo fue hecho")
-   meses_ultimo_pago = st.number_input("Numero de  meses desde efectuo su ultimo pago?")
-   meses_ultimo_credito = st.number_input("Numero de meses desde que LC obtuvo crédito por este préstamo")
+   meses_cr = st.number_input("Número de meses desde que se abrió la línea de crédito más temprana del prestatario")
+   meses_issue = st.number_input("Número de meses desde que el préstamo fue hecho")
+   meses_ultimo_pago = st.number_input("Número de  meses desde efectuo su ultimo pago?")
+   meses_ultimo_credito = st.number_input("Número de meses desde que LC obtuvo crédito por este préstamo")
 
    if grado=='A':
       gradeA=1
@@ -364,14 +262,6 @@ def main():
        st.sidebar.title("Su puntaje es superior o igual a la media")
    else:
        st.sidebar.title("Su puntaje es inferior a la media")
-
-
-
-
-
-
-
-
 
 if __name__=="__main__":
    main()
